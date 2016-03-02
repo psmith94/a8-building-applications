c9d63134c676e427fd07b1b823fc80731283db48
Pat Smith <Pat Smith>
2016-03-02 10:30:54 -0800

    Shiny app is working

--- server.R
+++ server.R
@@ -1,22 +1,30 @@
 #Patrick Smith
 #INFO498F
 #A8 Building an Application
-#server.R
+#This code hosts the Shiny server and builds the plotly app.
 
+#Setting golbal options and loading in R packages
 options(stringsAsFactors = FALSE)
 library(shiny)
 library(dplyr)
 library(plotly)
 
+#Loading in and saving the iris data.
 data("iris")
 
 iris_data <- iris
 
+#Server code, this code generates and displays a plotly plot on the mainpage of this Shiny app
 shinyServer(function(input, output) {
-  output$scatPlot <- renderPlot({
+  output$scatPlot <- renderPlotly({
     
+    #Plotly code. This chart is a dot plot
     plot_ly(iris_data, x = eval(parse(text = input$x_axis)), y = eval(parse(text = input$y_axis)),
             text = paste("Species: ", Species), mode = "markers",
-            color = Petal.Length, size = Petal.Width, opacity = Petal.Width)
+            color = Species, colors = input$`Color Set` ,size = Petal.Length, opacity = Petal.Width) %>% 
+      layout(title = 'Iris Measurement Plot',
+             xaxis = list(title = input$x_axis),
+              yaxis = list(title = input$y_axis)
+      )
   })
 })
--- ui.R
+++ ui.R
@@ -1,32 +1,46 @@
 #Patrick Smith
 #INFO 498F
 #A8 Building an Application
-#ui.R
+#The ui code for this Shiny app, several side widgets are created by this code.
 
+#Global settings and loading in needed packages.
 options(stringsAsFactors = FALSE)
 library(shiny)
 library(plotly)
 library(dplyr)
 
-
+#UI code
 shinyUI(fluidPage(
   
+  #Title of the Shiny app
   titlePanel("Iris App"),
   
+  #Code for widgets
   sidebarLayout(
     sidebarPanel(
+      
+      #Widget that controls the variable plotted on the x axis.
       selectInput("x_axis",
                   "Which value is displayed along the X axis",
-                  choices = list("Width" = 'Sepal.Width', "Length" = 'Sepal.Length')),
+                  choices = list("Width" = 'Sepal.Width', "Length" = 'Sepal.Length'),
+                  selected = 'Sepal.Length'),
     
-    
+      #Widget that controls the variable plotted on the y axis.
       selectInput("y_axis",
                   "Which Value is displayed along the y axis",
                   choices = list("Width" = 'Sepal.Width', "Length" = 'Sepal.Length'),
-                  selected = 'Sepal.Length')
+                  selected = 'Sepal.Width'),
+      
+      #Widget that controls the color set used in the Plotly plot.
+      selectInput("Color Set",
+                  "Select the color set to use",
+                  choices = list('Green' = 'Greens', 'Red' = 'Reds', 'Blue' = 'Blues'),
+                  selected = 'Reds')
+    
     ),
+    #Main pannel of the app, displays the plotly plot.
     mainPanel(
-      plotOutput("scatPlot")
+      plotlyOutput("scatPlot")
     )
   )
 ))
